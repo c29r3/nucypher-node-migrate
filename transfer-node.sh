a493adc21424228ac5f1375df31abeea9171e955
Sergey <52304443+c29r3@users.noreply.github.com>
2020-03-07 19:38:03 +0300

    Update transfer-node.sh

--- transfer-node.sh
+++ transfer-node.sh
@@ -43,7 +43,7 @@ echo -e $green"Extracting archive to home folder"
 cd $HOME && tar xzf $BACKUP_ARCHIVE
 
 echo -e $green"Creating virtual environment and install Nucpyher"
-python3 -m venv ~/nucypher-venv
+$(which python3) -m venv ~/nucypher-venv
 source ~/nucypher-venv/bin/activate
 pip3 install nucypher
 
