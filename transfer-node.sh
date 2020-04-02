b5e217c6b063789cab77b5b3069275bb39a6bc74
Sergey <52304443+c29r3@users.noreply.github.com>
2020-04-02 20:02:31 +0300

    Update transfer-node.sh

--- transfer-node.sh
+++ transfer-node.sh
@@ -94,7 +94,7 @@ User=$USER
 Type=simple
 Environment=NUCYPHER_KEYRING_PASSWORD=$KEYRING_PASSWORD
 Environment=NUCYPHER_WORKER_ETH_PASSWORD=$WORKER_ETH_PASSWORD
-ExecStart=$HOME/nucypher-venv/bin/nucypher ursula run --teacher discover.nucypher.network:9151 --poa
+ExecStart=$HOME/nucypher-venv/bin/nucypher ursula run --teacher gemini.nucypher.network:9151 --poa
 Restart=always
 RestartSec=5
 
